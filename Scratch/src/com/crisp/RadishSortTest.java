fe484c295860fba3f3193e32030444a5391be305
Michael W <welchm27@gmail.com>
2023-05-15 18:25:21 -0400

    Scratch Radish natural order (Comparable), Comparator and testing sorts

---
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -0,0 +1,30 @@
+package com.crisp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class RadishSortTest {
+    public static void main(String[] args) {
+        List<Radish> radishes = new ArrayList<>();
+        radishes.add(new Radish("red", 1.0, 2.2, 3));     // color, size, tailLength, greenThingies
+        radishes.add(new Radish("black", 3.5, 0.0, 0));
+        radishes.add(new Radish("red", 0.75, 3.1, 7));
+        radishes.add(new Radish("white", 1.0, 1.0, 2));
+
+        radishes.sort(null);    // passing null means natural order
+        System.out.println("Natural order (size)");
+        dump(radishes);
+        System.out.println();
+
+        radishes.sort(new RadishColorComparator());
+        System.out.println("Sorted by color");
+        dump(radishes);
+
+    }
+
+    private static void dump(List<Radish> radishList) {
+        for (Radish radish : radishList) {
+            System.out.println(radish);
+        }
+    }
+}
