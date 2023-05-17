7c15e063626e18cd91c119ec55b2310a7764260a
Michael W <welchm27@gmail.com>
2023-05-17 18:16:21 -0400

    Lab04.2 (Writing generic classes) complete

--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,15 +12,38 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
-  // storage for the cart's contents
-  private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
-  
-  public void addItem(T item) {
-    items.add(item);
-  }
-  
-  public void removeItem(T item) {
-    items.remove(item);
-  }
+/*
+ * Keep in mind, at runtime, client-side code is going to say something like this:
+ * ShoppingCart<Product>   T becomes Product, everywhere there is a T it is Product
+ * ShoppingCart<MusicItem>  T becomes MusicItem, everywhere there is a T....
+ * ShoppingCart<MediaPlayer>
+ */
+
+public class ShoppingCart<T extends Product> {
+    // storage for the cart's contents
+    private final Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+    public double total() {
+        double result = 0.0;
+        for (T item : items) {
+            result += item.getPrice();
+        }
+        return result;
+    }
+
+    public Collection<T> allItems() {
+        return Collections.unmodifiableCollection(items);
+    }
+
+    public int size() {
+        return items.size();
+    }
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public void removeItem(T item) {
+        items.remove(item);
+    }
 }
