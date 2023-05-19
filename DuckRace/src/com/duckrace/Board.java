9c5b3b765d61331ffdca2ff39bd87ec5fcb9aa53
Michael W <welchm27@gmail.com>
2023-05-19 15:37:11 -0400

    DuckRace add/find duckRacer and add/show prizes

--- DuckRace/src/com/duckrace/Board.java
+++ DuckRace/src/com/duckrace/Board.java
@@ -3,10 +3,8 @@ package com.duckrace;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.sql.SQLOutput;
+import java.util.*;
 
 /*
  * This is a lookup table of ids to student names.
@@ -45,9 +43,41 @@ class Board {
     private final Map<Integer,String> studentIdMap = loadStudentIdMap();
     private final Map<Integer,DuckRacer> racerMap  = new TreeMap<>();
 
+    /*
+     * Updates the board (racerMap) by making a DuckRacer "win."
+     * This could mean fetching an existing DuckRacer from racerMap,
+     * or we might need to create a new DuckRacer and put it in the map.
+     * Either way, we need to make it "win."
+     */
+    public void update(int id, Reward reward) {
+        DuckRacer racer = null;
+        if (racerMap.containsKey(id)){      // id exists in racerMap, so get DuckRacer next to it
+            racer = racerMap.get(id);
+        }
+        else {
+            racer = new DuckRacer(id, studentIdMap.get(id));
+            racerMap.put(id,racer);
+        }
+        racer.win(reward);
+    }
+
     // FOR TESTING PURPOSES
-    void dumpStudentIdMap() {
-        System.out.println(studentIdMap);
+    // TODO: render this data "pretty," for display to the end user
+    // see Java part 1, Session 5 for fixed width for text fields
+    // show the DuckRacers (only), ie., the right side of the map
+    void show() {
+        Collection<DuckRacer> racers = racerMap.values();
+
+        System.out.println("Duck Race Results");
+        System.out.println("=================\n");
+
+        System.out.println("id         name     wins     rewards");
+        System.out.println("__         ____     ____     _______");
+
+        for (DuckRacer racer : racers) {
+            System.out.printf("%2s    %10s     %2s     %s \n",
+                    racer.getId(), racer.getName(),racer.getWins(),racer.getRewards());
+        }
     }
 
     private Map<Integer, String> loadStudentIdMap() {
--- DuckRace/src/com/duckrace/BoardTest.java
+++ DuckRace/src/com/duckrace/BoardTest.java
@@ -5,7 +5,17 @@ class BoardTest {
     public static void main(String[] args) {
         // create a board object which initializes its fields, causing the file to get read
         Board board = new Board();
-        board.dumpStudentIdMap();
+
+        // update the board a few times, each time requires an id and a reward
+        board.update(3, Reward.PRIZES);
+        board.update(3, Reward.DEBIT_CARD);
+        board.update(10,Reward.DEBIT_CARD);
+        board.update(13, Reward.DEBIT_CARD);
+        board.update(1,Reward.PRIZES);
+        board.update(5,Reward.DEBIT_CARD);
+        board.update(3, Reward.DEBIT_CARD);
+
+        board.show();
 
     }
 }
