856b133c9b295100672ed78474844589ef7303c6
Michael W <welchm27@gmail.com>
2023-05-19 12:44:12 -0400

    Lab05.3 (TaxCalculator strategy) complete

--- Lab05.3/src/com/javatunes/billing/EuropeTax.java
+++ Lab05.3/src/com/javatunes/billing/EuropeTax.java
@@ -10,11 +10,24 @@ package com.javatunes.billing;
 
 /**
  * European orders are taxed as follows:
- *  VAT is 17% of taxable amount.
- *  Luxury tax is an additional 25% on amount over $100.
- *  
- * TODO: implement this algorithm.
+ * VAT is 17% of taxable amount.
+ * Luxury tax is an additional 25% on amount over $100.
  */
-public class EuropeTax {
+public class EuropeTax implements TaxCalculator {
+    private static final double vatTax = .17;
+    private static final double luxuryTax = .25;
+    private static final double luxuryFreeAmount = 100;
 
+    @Override
+    public double taxAmount(double taxable) {
+        double result = 0.0;
+
+        if (taxable <= luxuryFreeAmount) {
+            result = taxable * vatTax;
+        }
+        else {
+            result = (taxable * vatTax) + ((taxable - luxuryFreeAmount) * luxuryTax);
+        }
+        return result;
+    }
 }
