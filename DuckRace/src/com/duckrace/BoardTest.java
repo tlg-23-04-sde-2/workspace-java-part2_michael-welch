82a04e6195b0a29bf01b91295f353586beabec35
Michael W <welchm27@gmail.com>
2023-05-19 13:39:11 -0400

    DuckRace Load student-ids.csv into studentID map

---
+++ DuckRace/src/com/duckrace/BoardTest.java
@@ -0,0 +1,11 @@
+package com.duckrace;
+
+class BoardTest {
+
+    public static void main(String[] args) {
+        // create a board object which initializes its fields, causing the file to get read
+        Board board = new Board();
+        board.dumpStudentIdMap();
+
+    }
+}
